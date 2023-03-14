743a8a5c7003fb288d8ae98999528b5e77abe0d4
caiobga99 <caiobga99@gmail.com>
2023-03-14 15:25:43 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
